b3c6b68eb52e3c60c4ae17b85e6790a50ef8f212
g6520 <aaron.tu@evertrust.com.tw>
2021-10-07 15:16:36 +0800

    data/methods/v-on:click/v-text/v-html

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
-const app = Vue.createApp({
-  data() {
-    return {
-      rawHtml: `<span style="color: red">This should be red.</span>`,
-    };
+const vm = new Vue({
+  el: `.card`,
+  data: {
+    // 宣告變數
+    message: `Hello Vue!`,
+    rawHtml: `<p>Raw HTML</p>`,
+  },
+  methods: {
+    // 宣告事件處理
+    remove: function (e) {
+      this.message = "Removed!";
+      this.rawHtml = `<ol><li>1</li></ol>`;
+    },
   },
 });
-
-const vm = app.mount(".card");
