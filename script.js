ec37d68b7b10b78c9ceaa3f4d04abe39950afe7e
g6520 <aaron.tu@evertrust.com.tw>
2021-10-07 09:47:42 +0800

    建立component

--- script.js
+++ script.js
@@ -1,65 +1,8 @@
-const Counter = {
-  data() {
-    return {
-      counter: 0,
-    };
-  },
-  // mounted() {
-  //   setInterval(() => {
-  //     this.counter++;
-  //   }, 1000);
-  // },
-};
-Vue.createApp(Counter).mount(".counter");
+// Create Vue App
+const app = Vue.createApp({});
 
-const AttributeBinding = {
-  data() {
-    return {
-      message: `You loaded this page on ${new Date().toLocaleString()}`,
-      hint: `asdfasdfasf`,
-      description: `Test String`,
-    };
-  },
-};
-Vue.createApp(AttributeBinding).mount(".bind-attribute");
+app.component("item", {
+  template: `<p>Test</p>`,
+});
 
-const EventHandling = {
-  data() {
-    return {
-      message: `Hello Vue.js`,
-    };
-  },
-  methods: {
-    reverseMessage() {
-      this.message = this.message.split("").reverse().join("");
-    },
-  },
-};
-Vue.createApp(EventHandling).mount(".event-handling");
-
-const TwoWayBinding = {
-  data() {
-    return {
-      message: "Hello Vue!",
-    };
-  },
-};
-Vue.createApp(TwoWayBinding).mount(".two-way-binding");
-
-const ConditionRendering = {
-  data() {
-    return {
-      seen: false,
-    };
-  },
-};
-Vue.createApp(ConditionRendering).mount(".condition-rendering");
-
-const ListRendering = {
-  data() {
-    return {
-      todos: [{ text: "A" }, { text: "B" }, { text: "C" }],
-    };
-  },
-};
-Vue.createApp(ListRendering).mount(".list-rendering");
+app.mount(".card");
