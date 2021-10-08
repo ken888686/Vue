72c130f007696eeaf44f7c256abf5918503ca5bf
g6520 <aaron.tu@evertrust.com.tw>
2021-10-08 15:48:35 +0800

    v-on

--- script.js
+++ script.js
@@ -2,24 +2,26 @@ const passScore = 60;
 const app = new Vue({
   el: ".container",
   data: {
-    list1: [
-      { name: "A", id: 1, score: 66 },
-      { name: "B", id: 2, score: 82 },
-      { name: "C", id: 3, score: 65 },
-      { name: "D", id: 4, score: 59 },
-    ],
-    list2: {
-      Mary: {
-        phone: "0912345678",
-      },
-      sandy: {
-        phone: "0912345123",
-      },
-    },
+    count: 1,
   },
   methods: {
-    isPassed(score) {
-      return score > passScore;
+    add() {
+      this.count++;
+    },
+    warn(message, event) {
+      if (event) {
+        event.preventDefault();
+      }
+      alert(message);
+    },
+    dodo() {
+      alert(`dodo`);
+    },
+    doThis() {
+      alert(`doThis`);
+    },
+    doThat() {
+      alert(`doThat`);
     },
   },
 });
