58cd6f1ac46f40004f8bd7b680b6d6c4752beef6
g6520 <aaron.tu@evertrust.com.tw>
2021-10-08 10:30:26 +0800

    v-if/v-show

--- script.js
+++ script.js
@@ -1,29 +1,6 @@
 const app = new Vue({
   el: ".container",
   data: {
-    A: ``,
-    B: ``,
-    C: ``,
-    total: 0,
-    done: false,
-  },
-  computed: {
-    filterNum() {
-      if (this.A >= 100) {
-        this.A = ``;
-        alert(`Please under 100`);
-      }
-    },
-  },
-  methods: {
-    sum() {
-      this.total = `(A x ${this.A}) + (B x ${this.B}) + (C x ${this.C}) = ${
-        this.A * 50 + this.B * 25 + this.C * 10
-      }`;
-      this.A = ``;
-      this.B = ``;
-      this.C = ``;
-      this.done = true;
-    },
+    proMember: false,
   },
 });
