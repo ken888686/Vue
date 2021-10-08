e353c8fcc6bc2e64f89b6f61b077bf6e50919531
g6520 <aaron.tu@evertrust.com.tw>
2021-10-08 17:49:36 +0800

    $emit

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
 Vue.component("counter", {
   template: `
     <div>
-      Child - {{message}}
+      <p>Child - {{message}}</p>
+      <div>
+        <input type="button" v-on:click="returnData" value="Child return">
+      </div>
     </div>
   `,
   props: ["message"],
+  data() {
+    return {
+      text: "Got it!!",
+    };
+  },
+  methods: {
+    returnData() {
+      this.$emit("responsedata", this.text);
+    },
+  },
 });
 
 const app = new Vue({
   el: `.container`,
   data: {
     msg: "Parent",
+    receive: "Nothing",
+  },
+  methods: {
+    getChildMessage(context) {
+      this.receive = context;
+    },
   },
 });
