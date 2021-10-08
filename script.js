f0cfa7ffc5111f19bc8a7f3a2fa97e3807b7d9b1
g6520 <aaron.tu@evertrust.com.tw>
2021-10-08 11:14:34 +0800

    v-for

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
+const passScore = 60;
 const app = new Vue({
   el: ".container",
   data: {
-    proMember: false,
+    list1: [
+      { name: "A", id: 1, score: 66 },
+      { name: "B", id: 2, score: 82 },
+      { name: "C", id: 3, score: 65 },
+      { name: "D", id: 4, score: 59 },
+    ],
+    list2: {
+      Mary: {
+        phone: "0912345678",
+      },
+      sandy: {
+        phone: "0912345123",
+      },
+    },
+  },
+  methods: {
+    isPassed(score) {
+      return score > passScore;
+    },
   },
 });
